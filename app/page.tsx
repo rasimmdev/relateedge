fd66055d4da9b6c718d7735d65bc6240802be9cd
Rasim Matsko <rasimmatsko0@gmail.com>
2026-04-15 01:48:31 +0300

    Init commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-1/2 h-1/2 bg-red-500">Test</div>
+    </div>  
+   
+  );
+}
